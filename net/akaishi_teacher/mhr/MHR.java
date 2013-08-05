83adda36be38572c1f8ea75e57100fd099f709f1
mozipi <mozipi_2@yahoo.co.jp>
2013-08-05 23:33:53 +0900

    初期コミット

---
+++ net/akaishi_teacher/mhr/MHR.java
@@ -0,0 +1,27 @@
+package net.akaishi_teacher.mhr;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MHR extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		return super.onCommand(sender, command, label, args);
+	}
+
+
+
+}

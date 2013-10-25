caf7033ab783696691f4a0df4d051ac11f8290b3
mozipi <mozipi@mozipi-NV55C>
2013-10-25 18:50:26 +0900

    ビルド番号追加。あまり使わないけど、githubに上げてるので

--- Copy.sh
+++ Copy.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cp "/home/mozipi/ドキュメント/minecraft/plugins_develop/MHR/target/MineHorseRacing-1.0.1_for_164.jar" "/home/mozipi/ドキュメント/mcserver/debugserver/plugins/MineHorseRacing.jar"
+rm /home/mozipi/ドキュメント/mcserver/debugserver/plugins/*.jar
+cd target/
+cp *.jar /home/mozipi/ドキュメント/mcserver/debugserver/plugins
